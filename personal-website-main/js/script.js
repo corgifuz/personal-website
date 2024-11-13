450f6d71a2409085435c8b6846a55a365290de97
gillian <119890726+corgifuz@users.noreply.github.com>
2024-11-12 22:52:23 -0500

    Add files via upload

---
+++ personal-website-main/js/script.js
@@ -0,0 +1,224 @@
+
+/* ====== NATURALS ====== */
+
+const btnC = document.querySelector("#c");
+
+btnC.addEventListener('click', () => {
+    var audio = new Audio("keynotes/C.wav");
+    audio.play();
+})
+
+const btnD = document.querySelector("#d");
+
+btnD.addEventListener('click', () => {
+    var audio = new Audio("keynotes/D.wav");
+    audio.play();
+})
+
+const btnE = document.querySelector("#e");
+
+btnE.addEventListener('click', () => {
+    var audio = new Audio("keynotes/E.wav");
+    audio.play();
+})
+
+const btnF = document.querySelector("#f");
+
+btnF.addEventListener('click', () => {
+    var audio = new Audio("keynotes/F.wav");
+    audio.play();
+})
+
+const btnG = document.querySelector("#g");
+
+btnG.addEventListener('click', () => {
+    var audio = new Audio("keynotes/G.wav");
+    audio.play();
+})
+
+const btnA = document.querySelector("#a");
+
+btnA.addEventListener('click', () => {
+    var audio = new Audio("keynotes/A.wav");
+    audio.play();
+})
+
+const btnB = document.querySelector("#b");
+
+btnB.addEventListener('click', () => {
+    var audio = new Audio("keynotes/B.wav");
+    audio.play();
+})
+
+
+/* ========== HALF SHARPS ========== */
+
+
+const btn8 = document.querySelector("#b8");
+
+btn8.addEventListener('click', () => {
+    var audio = new Audio("keynotes/8.wav");
+    audio.play();
+})
+
+const btn7 = document.querySelector("#aa");
+
+btn7.addEventListener('click', () => {
+    var audio = new Audio("keynotes/7.wav");
+    audio.play();
+})
+
+const btn6 = document.querySelector("#gg");
+
+btn6.addEventListener('click', () => {
+    var audio = new Audio("keynotes/6.wav");
+    audio.play();
+})
+
+const btn5 = document.querySelector("#ff");
+
+btn5.addEventListener('click', () => {
+    var audio = new Audio("keynotes/5.wav");
+    audio.play();
+})
+
+const btn4 = document.querySelector("#ee");
+
+btn4.addEventListener('click', () => {
+    var audio = new Audio("keynotes/4.wav");
+    audio.play();
+})
+
+const btn3 = document.querySelector("#dd");
+
+btn3.addEventListener('click', () => {
+    var audio = new Audio("keynotes/3.wav");
+    audio.play();
+})
+
+const btn2 = document.querySelector("#cc");
+
+btn2.addEventListener('click', () => {
+    var audio = new Audio("keynotes/2.wav");
+    audio.play();
+})
+
+const btn1 = document.querySelector("#bb");
+
+btn1.addEventListener('click', () => {
+    var audio = new Audio("keynotes/1.wav");
+    audio.play();
+})
+
+
+/* ========== SHARPS ========== */
+
+const btnAa = document.querySelector("#aaa");
+
+btnAa.addEventListener('click', () => {
+    var audio = new Audio("keynotes/AA.wav");
+    audio.play();
+})
+
+const btnGg = document.querySelector("#ggg");
+
+btnGg.addEventListener('click', () => {
+    var audio = new Audio("keynotes/GG.wav");
+    audio.play();
+})
+
+const btnFf = document.querySelector("#fff");
+
+btnFf.addEventListener('click', () => {
+    var audio = new Audio("keynotes/FF.wav");
+    audio.play();
+})
+
+const btnEe = document.querySelector("#eee");
+
+btnEe.addEventListener('click', () => {
+    var audio = new Audio("keynotes/EE.wav");
+    audio.play();
+})
+
+const btnDd = document.querySelector("#ddd");
+
+btnDd.addEventListener('click', () => {
+    var audio = new Audio("keynotes/DD.wav");
+    audio.play();
+})
+
+const btnCc = document.querySelector("#ccc");
+
+btnCc.addEventListener('click', () => {
+    var audio = new Audio("keynotes/CC.wav");
+    audio.play();
+})
+
+const btnBb = document.querySelector("#bbb");
+
+btnBb.addEventListener('click', () => {
+    var audio = new Audio("keynotes/BB.wav");
+    audio.play();
+})
+
+
+
+
+/* ====== Sharp Sharps ========= */
+
+const btn16 = document.querySelector("#a16");
+
+btn16.addEventListener('click', () => {
+    var audio = new Audio("keynotes/16.wav");
+    audio.play();
+})
+
+const btn15 = document.querySelector("#gggg");
+
+btn15.addEventListener('click', () => {
+    var audio = new Audio("keynotes/15.wav");
+    audio.play();
+})
+
+const btn14 = document.querySelector("#ffff");
+
+btn14.addEventListener('click', () => {
+    var audio = new Audio("keynotes/14.wav");
+    audio.play();
+})
+
+const btn13 = document.querySelector("#eeee");
+
+btn13.addEventListener('click', () => {
+    var audio = new Audio("keynotes/13.wav");
+    audio.play();
+})
+
+const btn12 = document.querySelector("#dddd");
+
+btn12.addEventListener('click', () => {
+    var audio = new Audio("keynotes/12.wav");
+    audio.play();
+})
+
+const btn11 = document.querySelector("#cccc");
+
+btn11.addEventListener('click', () => {
+    var audio = new Audio("keynotes/11.wav");
+    audio.play();
+})
+
+const btn10 = document.querySelector("#bbbb");
+
+btn10.addEventListener('click', () => {
+    var audio = new Audio("keynotes/10.wav");
+    audio.play();
+})
+
+const btn9 = document.querySelector("#aaaa");
+
+btn9.addEventListener('click', () => {
+    var audio = new Audio("keynotes/9.wav");
+    audio.play();
+})
